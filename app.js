cbdbf06c1a48ef841ba178b16cc93d00ab36083d
acz19 <acz1992@hotmail.co.uk>
2023-06-06 22:13:34 +0200

    Delete commented completed tasks at top of app.js file

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// Remaining Jobs
-// 1) Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad
-//                Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
-//
-// 2) Instead of just changing the color of a square from black to white (for example), have each pass through with the mouse change it to a completely random RGB value. Then try having each pass just add another 10% of black to it so that only after 10 passes is the square completely black.
+// 1) Instead of just changing the color of a square from black to white (for example), have each pass through with the mouse change it to a completely random RGB value. Then try having each pass just add another 10% of black to it so that only after 10 passes is the square completely black.
 
 const gridContainer = document.querySelector(".gridContainer");
 const newGridButton = document.querySelector("#newGridButton");
